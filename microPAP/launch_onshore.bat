215e02f03c383aeb84521a818efcc1175e4274af
MartheH <122271502+MartheH@users.noreply.github.com>
2025-07-25 11:58:14 +0200

    Update launch_onshore.bat

--- microPAP/launch_onshore.bat
+++ microPAP/launch_onshore.bat
@@ -5,7 +5,7 @@ REM If Python is on your PATH, this will spawn each one in its own window
 echo Starting on-shore services...
 
 start "Dummy GPS"       python "%~dp0mqtt_dummy_gps.py"
-start "HiPOS Interface" py -3.13 "%~dp0hipos_interface2.py"
+start "HiPOS Interface" py -3.13 "%~dp0hipos_interface.py"
 start "Dummy ROV Sub"   python "%~dp0mqtt_dummy_sub_rov.py"
 
 echo All scripts launched.
